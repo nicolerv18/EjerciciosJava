3e985c4456acf0ecf5ba770ff9c263fd2b701ed1
Nicole <nicolerv18007@gmail.com>
2025-04-05 09:17:38 -0500

    Delete Matrices y funciones directory

--- Matrices y funciones/fuction/force/js/forces.js
+++
@@ -1,8 +0,0 @@
-let acceleration;
-function calculation(force, bulk){
-  acceleration= force /bulk;
- return acceleration;
-}
- 
- acceleration= calculation(75,3);
-console.log("The acceleration is " +acceleration);
--- Matrices y funciones/fuction/name/js/name.js
+++
@@ -1,6 +0,0 @@
-
-function greeting(name , age) {
-   console.log("hello " +name+ ",  you are " +age+ " years old");
-}
-
-name= greeting('juan', 13);
